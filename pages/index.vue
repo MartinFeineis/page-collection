7c8b47afdbcc4d301fafe1ee01fb648286472afe
Martin Feineis <martifein@gmail.com>
2023-10-14 17:46:40 -0400

    Linking 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Pages Index page</h1>
-    <Nlink to="/mortgage" >Mortgage calculator</Nlink>
+    <NuxtLink to="/mortgage" >Mortgage calculator</NuxtLink>
     <AppAlert>
       This is an auto-imported component
     </AppAlert>
