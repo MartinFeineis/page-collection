af2ffcada6231a4b12e3ab130c9ba82a4fe81977
Martin Feineis <martifein@gmail.com>
2023-10-15 12:57:06 -0400

    adding input

--- components/Mortgage.vue
+++ components/Mortgage.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <form @submit.prevent="calculateMortgage">
+        <input v-model="OutstandingMortgage" type="text" class="form-control" placeholder="500.000" aria-label="" aria-describedby="basic-addon1">
+    </form>
     <h2>{{ name }}</h2>
     <p>{{ message }}</p>
   </div>
