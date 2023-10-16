880f36e287f9c6a3a2247b0f5f1752ded4d29341
Martin Feineis <martifein@gmail.com>
2023-10-16 21:41:43 +0000

    added Link

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Pages Index page</h1>
     <NuxtLink to="/mortgage" >Mortgage calculator</NuxtLink>
+    <NuxtLink to="/qrscan" >QR Code Scanner</NuxtLink>
     <AppAlert>
       This is an auto-imported component
     </AppAlert>
