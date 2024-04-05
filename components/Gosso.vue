19a0d4a950d7cddb0f1c8a4f1e51a32c23db19c4
Martin Feineis <martifein@gmail.com>
2024-04-05 18:10:56 -0400

    using sso plugin

--- components/Gosso.vue
+++ components/Gosso.vue
@@ -1,35 +1,27 @@
-<template>
-    <div>
-      <div class="g-signin2" data-onsuccess="onSignIn"></div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'GoogleSignIn',
-    methods: {
-      onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-      }
-    },
-    mounted() {
-      gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': this.onSignIn,
-      });
-    }
-  }
-  </script>
-  
-  <style scoped>
-  </style>
+<script setup lang="ts">
+import {
+  GoogleSignInButton,
+  type CredentialResponse,
+} from "vue3-google-signin";
 
-<style></style>
+// handle success event
+const handleLoginSuccess = (response: CredentialResponse) => {
+  const { credential } = response;
+  console.log("Access Token", credential);
+};
+
+// handle an error event
+const handleLoginError = () => {
+  console.error("Login failed");
+};
+</script>
+
+<template>
+  <div>
+    <p><h3>Hello google Signin</h3></p>
+  </div>
+  <GoogleSignInButton
+    @success="handleLoginSuccess"
+    @error="handleLoginError"
+  ></GoogleSignInButton>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,23 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   plugins: ['~/plugins/qr'],
+  modules: [
+    'nuxt-vue3-google-signin'
+  ],
+  googleSignIn: {
+    clientId: '969151254190-pl534uplm25np58ajk2fi93hhidqp4pn.apps.googleusercontent.coms',
+  },
   app: {
     head: {
       link: [
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css', integrity: 'sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor', crossorigin: 'anonymous' }
       ],
-      meta: [
-        { hid: 'google-signin-client_id', name: 'google-signin-client_id', content: '969151254190-pl534uplm25np58ajk2fi93hhidqp4pn.apps.googleusercontent.coms' }
-      ],
+      // meta: [
+      //   { hid: 'google-signin-client_id', name: 'google-signin-client_id', content: '969151254190-pl534uplm25np58ajk2fi93hhidqp4pn.apps.googleusercontent.coms' }
+      // ],
       script: [
         { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', integrity: 'sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2', crossorigin: 'anonymous' },
-        { src: 'https://apis.google.com/js/platform.js', async: true, defer: true }
+        // { src: 'https://apis.google.com/js/platform.js', async: true, defer: true }
       ]
     }
   }
