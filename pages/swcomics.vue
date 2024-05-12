baceb26391cd72c8dd80f20e503382cd701338f1
Martin Feineis <martifein@gmail.com>
2024-05-12 18:21:25 -0400

    works locally

--- pages/swcomics.vue
+++ pages/swcomics.vue
@@ -1,20 +1,20 @@
-<script>
-    console.log("Hello JS World!")
-    import { sql } from '@vercel/postgres';
- 
-const likes = 100;
-const { rows, fields } =
-  await sql`SELECT * FROM sw_comics;`;
+<script setup>
+console.log("Hello JS World!")
+import { db, sql } from '@vercel/postgres';
+
+var comics = await sql`SELECT * FROM sw_comics;`;
+console.log(comics.rows)
 </script>
 <style>
-    h1 {
-        background-color: #9932CC;
-    }
+h1 {
+    background-color: #9932CC;
+}
 </style>
 <template>
     <div>
         <h1>Hello New Component</h1>
-        <p>{{ rows }}</p>
-        <p>{{ fields }}</p>
+        <li v-for="item in comics.rows">
+            <p>{{ item }}</p>>
+        </li>
     </div>
 </template>
