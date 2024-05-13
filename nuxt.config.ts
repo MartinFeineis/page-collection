40f7c664b0e4987b380f5b1eb7152d133f9237b0
Martin Feineis <martifein@gmail.com>
2024-05-13 18:40:16 -0400

    nuxt routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  routeRules: {
+    '/swcomics': { ssr: true },
+  },
   plugins: ['~/plugins/qr'],
   modules: [
     'nuxt-vue3-google-signin'
