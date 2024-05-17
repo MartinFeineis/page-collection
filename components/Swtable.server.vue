b69b7d2da3fa0e6d2a8bcc290b5e4cb04ecd6685
Martin Feineis <martifein@gmail.com>
2024-05-17 09:11:54 -0400

    refactored vue sw_component

---
+++ components/Swtable.server.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Hello New Component</h1>
+    </div>
+    <div>
+        <table>
+            <th>Band Nr.</th>
+            <th>Titel</th>
+            <th>Notizen</th>
+            <tr v-for="item in comics.rows">
+                <td>{{ item.band }}</td>
+                <td>{{ item.title }}</td>
+                <td>{{ item.notes }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+import { ref } from 'vue';
+import { sql } from '@vercel/postgres';
+let comics = ref(null);
+
+async function fetchComics() {
+    comics = await sql`SELECT * FROM sw_comics;`;
+    console.log(comics);
+}
+comics = fetchComics()
+export default {
+    data(){
+        return{
+            comics
+        }
+    }
+}
+</script>
+<style>
+h1 {
+    background-color: #9932CC;
+}
+</style>
--- pages/swcomics.vue
+++ pages/swcomics.vue
@@ -1,40 +1,15 @@
 <template>
     <div>
-        <h1>Hello New Component</h1>
+        <h1 class="component">Hello New Page</h1>
     </div>
     <div>
-        <table>
-            <th>Band Nr.</th>
-            <th>Titel</th>
-            <th>Notizen</th>
-            <tr v-for="item in comics.rows">
-                <td>{{ item.band }}</td>
-                <td>{{ item.title }}</td>
-                <td>{{ item.notes }}</td>
-            </tr>
-        </table>
+       <Swtable />
     </div>
 </template>
 <script>
-import { ref } from 'vue';
-import { sql } from '@vercel/postgres';
-let comics = ref(null);
-
-async function fetchComics() {
-    comics = await sql`SELECT * FROM sw_comics;`;
-    console.log(comics);
-}
-comics = fetchComics()
-export default {
-    data(){
-        return{
-            comics
-        }
-    }
-}
 </script>
 <style>
-h1 {
-    background-color: #9932CC;
+h1.component {
+    background-color: #e61414;
 }
 </style>
