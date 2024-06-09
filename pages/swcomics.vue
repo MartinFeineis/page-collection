aa9b82cd51e7c2c7218b93a09242e0a137c4d430
Martin Feineis <martifein@gmail.com>
2024-06-09 12:28:58 -0400

    fixed listing etc

--- pages/swcomics.vue
+++ pages/swcomics.vue
@@ -1,31 +1,42 @@
 <script setup>
-const comics = await useFetch('/api/comics')
-console.log(comics)
+import { ref, onMounted } from 'vue';
+import { useFetch } from '@vueuse/core'; // Make sure you have this or adjust the import based on your setup
+
+const comics = ref([]);
+
+async function fetchComics() {
+  const response = await useFetch('/api/comics');
+  comics.value = response.data._value.comics;
+}
+
+onMounted(fetchComics);
+
+console.log(comics);
 console.log(typeof comics);
 </script>
 
 <template>
-    <div>
-        <h1>Hello New Component</h1>
-        <p>List of my Starwars comicbooks</p>
-    </div>
-    <div>
-        <table id="swtable" class="table table-bordered table-dark">
-            <thead>
-                <tr>
-                    <th scope="col">Band Nr.</th>
-                    <th scope="col">Titel</th>
-                    <th scope="col">Notizen</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in comics.data._value.comics" :key="item.band>
-                <td>{{ item.band }}</td>
-                <td>{{ item.title }}</td>
-                <td>{{ item.notes }}</td>
-                <!-- <td>{{ item }} new element</td> -->
-            </tr>
-        </tbody>
-        </table>
-    </div>
+  <div>
+    <h1>Hello New Component</h1>
+    <p>List of my Starwars comicbooks</p>
+  </div>
+  <div>
+    <table id="swtable" class="table table-bordered table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Band Nr.</th>
+          <th scope="col">Titel</th>
+          <th scope="col">Notizen</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in comics" :key="item.band">
+          <td>{{ item.band }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.notes }}</td>
+          <!-- <td>{{ item }} new element</td> -->
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
