a895c50bb17fc610d2822a6b6e3955dd2ddc33ee
Martin Feineis <martifein@gmail.com>
2024-06-09 11:58:34 -0400

    login work

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <h1>Pages Index page</h1>
-    <NuxtLink to="/mortgage" >Mortgage calculator</NuxtLink>
-    <NuxtLink to="/qrscan" >QR Code Scanner</NuxtLink>
-    <NuxtLink to="/notes" >Notes</NuxtLink>
-    <NuxtLink to="/swcomics" >Star Wars Comics</NuxtLink>
-    <Appalert />
-  <Gosso />
+    <ul>
+    <li><NuxtLink to="/login" >Login</NuxtLink></li>
+    <li><NuxtLink to="/mortgage" >Mortgage calculator</NuxtLink></li>
+    <li><NuxtLink to="/qrscan" >QR Code Scanner</NuxtLink></li>
+    <li><NuxtLink to="/notes" >Notes</NuxtLink></li>
+    <li><NuxtLink to="/swcomics" >Star Wars Comics</NuxtLink></li>
+    <li><Appalert /></li>
+    <li><Gosso /></li>
+  </ul>
   </div>
 </template>
 
--- pages/swcomics.vue
+++ pages/swcomics.vue
@@ -1,5 +1,5 @@
 <script setup>
-const comics  = await useFetch('/api/comics')
+const comics = await useFetch('/api/comics')
 console.log(comics)
 console.log(typeof comics);
 </script>
@@ -11,14 +11,15 @@ console.log(typeof comics);
     </div>
     <div>
         <table id="swtable" class="table table-bordered table-dark">
-            <thead><tr>
-            <th scope="col">Band Nr.</th>
-            <th scope="col">Titel</th>
-            <th scope="col">Notizen</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="item in comics.data._value.comics">
+            <thead>
+                <tr>
+                    <th scope="col">Band Nr.</th>
+                    <th scope="col">Titel</th>
+                    <th scope="col">Notizen</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in comics.data._value.comics" :key="item.band>
                 <td>{{ item.band }}</td>
                 <td>{{ item.title }}</td>
                 <td>{{ item.notes }}</td>
