7c03b3f10fa4bf915b304c8d4de057e21d91e700
Martin Feineis <martifein@gmail.com>
2024-06-15 19:12:19 -0400

    troubleshooting?

--- components/Gosso.vue
+++ components/Gosso.vue
@@ -1,6 +1,9 @@
 <template>
-  <script src="https://apis.google.com/js/platform.js" async defer></script>
   <meta name="google-signin-client_id" content="969151254190-pl534uplm25np58ajk2fi93hhidqp4pn.apps.googleusercontent.com">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js">
+  </script>
+  <script src="https://apis.google.com/js/client:platform.js?onload=start" async defer>
+  </script>
   <div>
     <p><h3>Hello google Signin</h3></p>
   </div>
