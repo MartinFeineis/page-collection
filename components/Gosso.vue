69c603abf9d0be8829bca1a5734537753485d2cb
Martin Feineis <martifein@gmail.com>
2024-06-15 19:14:01 -0400

    troubleshooting 2

--- components/Gosso.vue
+++ components/Gosso.vue
@@ -1,12 +1,18 @@
 <template>
-  <meta name="google-signin-client_id" content="969151254190-pl534uplm25np58ajk2fi93hhidqp4pn.apps.googleusercontent.com">
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js">
-  </script>
-  <script src="https://apis.google.com/js/client:platform.js?onload=start" async defer>
-  </script>
-  <div>
-    <p><h3>Hello google Signin</h3></p>
-  </div>
-  <div class="g-signin2" data-onsuccess="onSignIn"></div>
-  <Loggedin />
+<div id="g_id_onload"
+     data-client_id="969151254190-pl534uplm25np58ajk2fi93hhidqp4pn.apps.googleusercontent.com"
+     data-context="signin"
+     data-ux_mode="popup"
+     data-login_uri="https://pages.rjo.me/"
+     data-auto_prompt="false">
+</div>
+
+<div class="g_id_signin"
+     data-type="standard"
+     data-shape="rectangular"
+     data-theme="outline"
+     data-text="signin_with"
+     data-size="large"
+     data-logo_alignment="left">
+</div>
 </template>
