58636a0c276e2cc1f2b2f31317589229f2b14bff
Martin Feineis <martifein@gmail.com>
2024-06-15 18:24:02 -0400

    fixed script for sigin

--- components/Gosso.vue
+++ components/Gosso.vue
@@ -1,6 +1,5 @@
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-
 <template>
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
   <meta name="google-signin-client_id" content="969151254190-pl534uplm25np58ajk2fi93hhidqp4pn.apps.googleusercontent.com">
   <div>
     <p><h3>Hello google Signin</h3></p>
