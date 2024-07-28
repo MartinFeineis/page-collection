5dcd6ae5baa97596182f6407c8b6d107d2a259d8
Martin Feineis <martifein@gmail.com>
2024-07-28 15:20:19 -0400

    Updating error pages

--- error.vue
+++ error.vue
@@ -1,7 +1,27 @@
 <script setup>
+import { ref, computed } from 'vue';
 const error = useError();
+
+const quotes = ref([
+  "Oops! Something went wrong.",
+  "Well, this is awkward.",
+  "Houston, we have a problem.",
+  "Whoops! That didn't work.",
+  "Error 404: Fun not found.",
+  "This is not the page you're looking for.",
+  "Something broke. We're on it!",
+  "Yikes! That wasn't supposed to happen.",
+  "Oh no! An error occurred.",
+  "Keep calm and try again."
+]);
+
+const randomQuote = computed(() => {
+  const index = Math.floor(Math.random() * quotes.value.length);
+  return quotes.value[index];
+});
 </script>
+
 <template>
-    <h1>That didn't work!</h1>
-    <h2>We got a {{  error.statusCode }}</h2>
+  <h1>{{ randomQuote }}</h1>
+  <h2>We got a {{ error.statusCode }}</h2>
 </template>
