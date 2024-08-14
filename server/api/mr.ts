0a13de30de8b4d3da3d81c0bd884562fa1b782e3
Martin Feineis <martifein@gmail.com>
2024-08-14 17:58:39 -0400

    now with slack?

--- server/api/mr.ts
+++ server/api/mr.ts
@@ -1,5 +1,24 @@
+import { WebClient } from '@slack/web-api';
 export default defineEventHandler((event) => {
   const query = getQuery(event)
+  // Create a new instance of the WebClient class with the token read from your environment variable
+  const web = new WebClient(process.env.SLACK_API_KEY);
+// The current date
+   const currentTime = new Date().toTimeString();
+   
 
-  return { msg: query.msg }
+  (async () => {
+
+    try {
+      // Use the `chat.postMessage` method to send a message from this app
+      await web.chat.postMessage({
+        channel: '#hacking',
+        text: query.msg,
+      });
+      console.log('Message posted!');
+    } catch (error) {
+      console.log(error);
+    }
+
+  })();  return { msg: query.msg }
 })
