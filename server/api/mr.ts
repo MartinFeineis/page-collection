5ba78e789f7351e1552e966fc8e52a2d8cd0c84d
Martin Feineis <martifein@gmail.com>
2024-08-15 09:59:47 -0400

    added error response

--- server/api/mr.ts
+++ server/api/mr.ts
@@ -1,26 +1,22 @@
 import { WebClient } from '@slack/web-api';
-export default defineEventHandler((event) => {
-  const query = getQuery(event)
-  // Create a new instance of the WebClient class with the token read from your environment variable
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
   const web = new WebClient(process.env.SLACK_API_KEY);
-// The current date
-   const currentTime = new Date().toTimeString();
-   
+  const currentTime = new Date().toTimeString();
+  
   let myerror = null;
-  (async () => {
 
-    try {
-      // Use the `chat.postMessage` method to send a message from this app
-      await web.chat.postMessage({
-        channel: '#hacking',
-        text: query.msg,
-      });
-      console.log('Message posted!');
-    } catch (error) {
-      myerror = error;
-      console.log(error);
-    }
+  try {
+    await web.chat.postMessage({
+      channel: '#hacking',
+      text: query.msg,
+    });
+    console.log('Message posted!');
+  } catch (error) {
+    myerror = error;
+    console.log(error);
+  }
 
-  })(); 
-   return { msg: query.msg , err: myerror};
-})
+  return { msg: query.msg, err: myerror };
+});
