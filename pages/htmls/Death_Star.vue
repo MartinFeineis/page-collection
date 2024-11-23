e9a842c93e953f619437482960bf2c1b2a2fc4f9
Martin Feineis <martifein@gmail.com>
2024-11-23 14:13:58 -0500

    with Notes

---
+++ pages/htmls/Death_Star.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="bodyContainer">
+      <h1>
+        <div class="notebookFor">Notes and highlights for</div>
+        <div class="bookTitle">Death Star: Star Wars Legends (Star Wars - Legends)</div>
+        <div class="authors">Reaves, Michael; Perry, Steve</div>
+      </h1>
+      <hr />
+      <div class="noteText">Death Star</div>
+      <hr />
+      <h2 class="sectionHeading">Part One - Construction</h2>
+  
+      <h3 class="noteHeading">
+        Highlight (<span class="highlight_yellow">yellow</span>) - Chapter 2 &gt; Page 14 · Location 322
+      </h3>
+      <div class="noteText">mundicidal</div>
+  
+      <!-- Repeat similar structure for all highlights as shown above -->
+  
+    </div>
+  </template>
+  
+  <style scoped>
+    .bodyContainer {
+      font-family: Arial, Helvetica, sans-serif;
+      text-align: center;
+      padding-left: 32px;
+      padding-right: 32px;
+    }
+  
+    .notebookFor {
+      font-size: 18px;
+      font-weight: 700;
+      text-align: center;
+      color: rgb(119, 119, 119);
+      margin: 24px 0px 0px;
+      padding: 0px;
+    }
+  
+    /* Additional CSS styles here */
+  </style>
+  
