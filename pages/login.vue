d5b012ccb429065d55accf2de929a6ed22bf6216
Martin Feineis <martifein@gmail.com>
2024-12-31 00:08:09 -0500

    with sso

--- pages/login.vue
+++ pages/login.vue
@@ -5,26 +5,36 @@
     </div>
     <div class="mt-10">
       <h2 class="text-5xl font-bold mb-7">Log in</h2>
-      <button>Login with Google</button>
+      <button @click="login">Login with Google</button>
     </div>
 </template>
-<script>
+
+<script setup>
 const supabase = useSupabaseClient();
-const user_profile = ref(null); // Initialize user_profile
-function onSignIn(googleUser) {
-  var user_profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-export default {
-  data() {
-    return {
-      profile: user_profile
-    };
+const login =  async () => {
+  const {error} = supabase.auth.signInWithOAuth({
+    provider: "google"
+  });
+
+  if (error) {
+    console.log(error)
   }
 };
+// const user_profile = ref(null); // Initialize user_profile
+// function onSignIn(googleUser) {
+//   var user_profile = googleUser.getBasicProfile();
+//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//   console.log('Name: ' + profile.getName());
+//   console.log('Image URL: ' + profile.getImageUrl());
+//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
+// export default {
+//   data() {
+//     return {
+//       profile: user_profile
+//     };
+//   }
+// };
 </script>
 <style>
     h1 {
