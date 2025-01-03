93273dfb84ebc0389d46476b53b72af5616340ba
Martin Feineis <martifein@gmail.com>
2025-01-03 11:47:12 -0500

    added logout

--- components/Home.vue
+++ components/Home.vue
@@ -1,11 +1,30 @@
+<script setup>
+const supabase = useSupabaseClient();
+const login =  async () => {
+  const {error} = supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+    redirectTo: `http://pages.rjo.me/login`,
+  },
+  });
+
+  if (error) {
+    console.log(error)
+  }
+};
+const user = useSupabaseUser()
+const logout = () => {
+    supabase.auth.signOut()
+}
+</script>
 <template>
     <div>
         <p><NuxtLink to="/">go Home</NuxtLink></p>
         <div v-if="user">
-            <NuxtLink to="/uprofile">Profile</NuxtLink>
+            <button @click="logout">logout</button>
         </div>
         <div v-else>
-            <NuxtLink to="Login">Login</NuxtLink>
+            <button @click="login">login</button>
         </div>
     </div>
 </template>
