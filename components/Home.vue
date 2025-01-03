8a7344b581c82f7af3c3e550837866d88609b6b2
Martin Feineis <martifein@gmail.com>
2025-01-03 11:37:43 -0500

    updated Home bar

--- components/Home.vue
+++ components/Home.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
         <p><NuxtLink to="/">go Home</NuxtLink></p>
+        <div v-if="user">
+            <NuxtLink to="/uprofile">Profile</NuxtLink>
+        </div>
+        <div v-else>
+            <NuxtLink to="Login">Login</NuxtLink>
+        </div>
     </div>
 </template>
