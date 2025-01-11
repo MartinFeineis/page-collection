b141fb563b274766d34961ce608087586b96ee8a
Martin Feineis <martifein@gmail.com>
2025-01-11 17:22:52 -0500

    another try

--- pages/gprofile/[[username]].vue
+++ pages/gprofile/[[username]].vue
@@ -1,7 +1,33 @@
 <script setup>
-const supabase = useSupabaseClient()
-loading.value = true
-const user = useSupabaseUser()
+import { useRouter } from 'vue-router'; // Ensure Vue Router is set up in your project
+const supabase = useSupabaseClient();
+const router = useRouter();
+
+// const loadProfile = async () => {
+//   const { data: user, error } = await supabase.auth.getUser();
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+  
+//   if (user) {
+//     const username = user.user_metadata.username || user.email.split('@')[0]; // Use username if available or fallback
+//     // Redirect to user-specific profile page
+//     router.push(`/gprofile/${username}`);
+//   }
+// };
+
+// const {
+//   data: { user },
+// } = await supabase.auth.getUser()
+// let metadata = user.user_metadata
+
+
+// // Load user profile when this page is visited
+// loadProfile();
+// const supabase = useSupabaseClient()
+// loading.value = true
+// const user = useSupabaseUser()
 
 const { data } = await supabase
   .from('profiles')
@@ -9,13 +35,13 @@ const { data } = await supabase
   .eq('id', user.value.id)
   .single()
 
-if (data) {
-  username.value = data.username
-  website.value = data.website
-  avatar_path.value = data.avatar_url
-}
+// if (data) {
+//   username.value = data.username
+//   website.value = data.website
+//   avatar_path.value = data.avatar_url
+// }
 
-loading.value = false
+// loading.value = false
 </script>
 
 <template>
