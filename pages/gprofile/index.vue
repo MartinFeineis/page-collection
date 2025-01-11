025acf3b672128229eab9177bab7dd661c7f3e08
Martin Feineis <martifein@gmail.com>
2025-01-11 17:41:16 -0500

    another try 4

--- pages/gprofile/index.vue
+++ pages/gprofile/index.vue
@@ -1,26 +1,26 @@
 <script setup>
-// import { useRouter } from 'vue-router';
-// import { useSupabaseClient } from '@supabase/vue';
+import { useRouter } from 'vue-router';
+import { useSupabaseClient } from '@supabase/vue';
 
-// const supabase = useSupabaseClient();
-// const router = useRouter();
+const supabase = useSupabaseClient();
+const router = useRouter();
 
-// const redirectToProfile = async () => {
-//   const { data: user, error } = await supabase.auth.getUser();
+const redirectToProfile = async () => {
+  const { data: user, error } = await supabase.auth.getUser();
 
-//   if (error) {
-//     console.log('Error fetching user:', error);
-//     return;
-//   }
+  if (error) {
+    console.log('Error fetching user:', error);
+    return;
+  }
 
-//   if (user) {
-//     const username = user.user_metadata.username || user.email.split('@')[0]; // Use a username or email prefix
-//     // Redirect to the dynamic profile page
-//     router.push(`/gprofile/${username}`);
-//   }
-// };
+  if (user) {
+    const username = user.user_metadata.username || user.email.split('@')[0]; // Use a username or email prefix
+    // Redirect to the dynamic profile page
+    router.push(`/gprofile/${username}`);
+  }
+};
 
-// redirectToProfile();
+redirectToProfile();
 </script>
 
 <template>
