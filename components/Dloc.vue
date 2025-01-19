1d936ad47e59c1de8540b4b2a8a7ae90228b7cd6
Martin Feineis <martifein@gmail.com>
2025-01-19 10:12:12 -0500

    adding Dloc

---
+++ components/Dloc.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+      <h1>User Location</h1>
+      <div v-if="error" class="error">{{ error }}</div>
+      <div v-else-if="location">
+        <p>Latitude: {{ location.latitude }}</p>
+        <p>Longitude: {{ location.longitude }}</p>
+      </div>
+      <button @click="fetchLocation">Get Location</button>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from "vue";
+  
+  export default {
+    name: "DisplayLocation",
+    setup() {
+      const location = ref(null);
+      const error = ref(null);
+  
+      const fetchLocation = async () => {
+        if (!navigator.geolocation) {
+          error.value = "Geolocation is not supported by your browser.";
+          return;
+        }
+  
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            location.value = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            };
+            error.value = null;
+          },
+          (err) => {
+            error.value = `Unable to retrieve location: ${err.message}`;
+          }
+        );
+      };
+  
+      return {
+        location,
+        error,
+        fetchLocation,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .error {
+    color: red;
+    font-weight: bold;
+  }
+  </style>
+  
