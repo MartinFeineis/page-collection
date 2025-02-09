83b6d5605411fa90b803c2399ddc7ae78d58a09a
Martin Feineis <martifein@gmail.com>
2025-02-09 12:38:47 -0500

    chatgpt fix 1

--- server/api/loc.get.ts
+++ server/api/loc.get.ts
@@ -1,12 +1,12 @@
 import { sql } from '@vercel/postgres'
-import { gridDiskDistances }from 'h3-js';
+import h3 from 'h3-js';
 
 const ringSize = 3
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const h3index = query.h3index as string;
-  const neighbors = gridDiskDistances(h3index, ringSize)
+  const neighbors = h3.gridDiskDistances(h3index, ringSize).flat();
 
   const { rows, fields } = await sql`SELECT * FROM locations WHERE h3index = ${h3index}`
   return {
