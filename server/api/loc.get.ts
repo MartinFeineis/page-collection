fe8a2223fed598c9ee14e2c84fdcd4eaa3dd6813
Martin Feineis <martifein@gmail.com>
2025-02-09 13:49:57 -0500

    removed h3-js in loc

--- server/api/loc.get.ts
+++ server/api/loc.get.ts
@@ -1,15 +1,15 @@
 import { sql } from '@vercel/postgres'
-import h3 from 'h3-js';
+//import h3 from 'h3-js';
 
 const ringSize = 3
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const h3index = query.h3index as string;
-  const neighbors = h3.gridDiskDistances(h3index, ringSize).flat();
+  //const neighbors = h3.gridDiskDistances(h3index, ringSize).flat();
 
   const { rows, fields } = await sql`SELECT * FROM locations WHERE h3index = ${h3index}`
   return {
-    'h3': h3index, 'cur': rows, 'neighbors': neighbors, 'fields': fields
+    'h3': h3index, 'cur': rows //, 'neighbors': neighbors, 'fields': fields
   }
 });
