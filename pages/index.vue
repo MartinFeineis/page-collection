8dd358893bb82c0796d2eceac19785fe6624b95a
Martin Feineis <martifein@gmail.com>
2025-02-09 14:00:54 -0500

    fixing runtime

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,6 @@
   </ul>
   <div><h4>Attributions <a href="https://simplemaps.com/data/us-cities">simplemaps</a></h4></div>
   </div>
-      <p>Last Commit: returned to manual import</p>
+      <p>Last Commit: fixing runtime</p>
 </template>
 
--- server/api/loc.get.ts
+++ server/api/loc.get.ts
@@ -1,11 +1,16 @@
 import { sql } from '@vercel/postgres'
-import { gridDiskDistances } from 'h3-js';
+import h3 from 'h3-js';
+
+export const config = {
+  runtime: 'nodejs', // Forces this route to run in Node.js instead of Edge
+};
+
+const ringSize = 3
 
 export default defineEventHandler(async (event) => {
-  const ringSize = 3
   const query = getQuery(event);
   const h3index = query.h3index as string;
-  const neighbors = gridDiskDistances(h3index, ringSize).flat();
+  const neighbors = h3.gridDiskDistances(h3index, ringSize).flat();
 
   const { rows, fields } = await sql`SELECT * FROM locations WHERE h3index = ${h3index}`
   return {
