ab1ec6c61b1c69b7159b6c548960f06cfb6f65d4
Martin Feineis <martifein@gmail.com>
2025-02-08 20:28:39 -0500

    loc fix

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,6 @@
   </ul>
   <div><h4>Attributions <a href="https://simplemaps.com/data/us-cities">simplemaps</a></h4></div>
   </div>
-      <p>Last Commit: adding http verb</p>
+      <p>Last Commit: loc fix</p>
 </template>
 
--- server/api/loc.get.ts
+++ server/api/loc.get.ts
@@ -1,5 +1,15 @@
+import { createPool, sql } from '@vercel/postgres'
+
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const h3index = query.h3index as string;
-  return {'h3': h3index }
+  const db = createPool()
+  try {
+    const { rows, fields } = await db.query('SELECT name FROM locations WHERE h3index = ${h3index}')
+    return {
+      'h3': h3index, 'cur': rows, 'fields': fields
+    }
+  } catch (error) {
+      throw error
+  }
 });
